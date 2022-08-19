97846468d6841544a16e69a315b3435e05f0b2a3
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-08-19 13:27:59 +0200

    wip: copy module headers over (& include them)

--- module/cpp/react-native-skia-module.cpp
+++ module/cpp/react-native-skia-module.cpp
@@ -1,9 +1,85 @@
-#include "react-native-skia-module.h"
-#include <JsiSkColor.h>
-#include <RNSkLog.h>
+#pragma once
 
-namespace example {
-	int multiply(float a, float b) {
-		return a * b;
-	}
+#include <JsiSkHostObjects.h>
+#include <JsiSkCanvas.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
+
+#include <modules/skottie/include/Skottie.h>
+
+#pragma clang diagnostic pop
+
+#include <jsi/jsi.h>
+
+namespace RNSkia {
+    using namespace facebook;
+
+    class JsiSkSkottieAnimation : public JsiSkWrappingSkPtrHostObject<skottie::Animation>
+    {
+    public:
+        //#region Properties
+        JSI_PROPERTY_GET(duration) { return static_cast<double>(getObject()->duration()); }
+        JSI_PROPERTY_GET(fps) { return static_cast<double>(getObject()->fps()); }
+
+        JSI_EXPORT_PROPERTY_GETTERS(JSI_EXPORT_PROP_GET(JsiSkSkottieAnimation, duration),
+                                    JSI_EXPORT_PROP_GET(JsiSkSkottieAnimation, fps))
+        //#endregion
+
+
+        //#region Methods
+        JSI_HOST_FUNCTION(seek) {
+            getObject()->seek(arguments[0].asNumber());
+            return jsi::Value::undefined();
+        }
+
+        JSI_HOST_FUNCTION(render) {
+            auto canvas = JsiSkCanvas::fromValue(runtime, arguments[0]);
+            auto rect = JsiSkRect::fromValue(runtime, arguments[1]);
+            if (canvas != nullptr && rect != nullptr)
+                getObject()->render(canvas, rect.get());
+
+            return jsi::Value::undefined();
+        }
+
+        JSI_EXPORT_FUNCTIONS(JSI_EXPORT_FUNC(JsiSkSkottieAnimation, seek),
+                             JSI_EXPORT_FUNC(JsiSkSkottieAnimation, render), )
+        //#endregion
+
+        /**
+          Constructor
+        */
+        JsiSkSkottieAnimation(std::shared_ptr<RNSkPlatformContext> context,
+                              const sk_sp<skottie::Animation> animation)
+                : JsiSkWrappingSkPtrHostObject<skottie::Animation>(std::move(context), std::move(animation)){}
+
+        /**
+          Returns the jsi object from a host object of this type
+        */
+        static sk_sp<skottie::Animation> fromValue(jsi::Runtime &runtime, const jsi::Value &obj) {
+            return obj.asObject(runtime)
+                    .asHostObject<JsiSkSkottieAnimation>(runtime)
+                    ->getObject();
+        }
+
+        /**
+         * Creates the function for contructing a new instance of the
+         * JsiSkSkottieAnimation class.
+         *
+         * @param context platform context
+         * @return A function for creating a new host object wrapper for the JsiSkSkottieAnimation class.
+         */
+        static const jsi::HostFunctionType
+        createCtor(std::shared_ptr<RNSkPlatformContext> context) {
+            return JSI_HOST_FUNCTION_LAMBDA {
+                auto jsonStr = arguments[0].asString(runtime).utf8(runtime);
+                auto animation = skottie::Animation::Builder()
+                        .make(jsonStr.c_str(), jsonStr.size());
+
+                // Return the newly constructed object
+                return jsi::Object::createFromHostObject(
+                        runtime, std::make_shared<JsiSkSkottieAnimation>(std::move(context), std::move(animation)));
+            };
+        }
+    };
 }
